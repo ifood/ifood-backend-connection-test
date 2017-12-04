14c900cb7f84f6011a24a4d431a4c2d6b1dfd570
Rafael <trestini@gmail.com>
2017-12-04 00:06:38 -0200

    Reestruturacao do logger

--- schedule-service/app/index.js
+++ schedule-service/app/index.js
@@ -1,29 +1,10 @@
-const winston = require('winston');
-
-let logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
-  ],
-  exitOnError: false
-});
-
-logger.stream = {
-  write: function (message) {
-    logger.log('info', '[morgan] ' + message.trim());
-  }
-};
-
-logger.info("Log configured sucessfully");
+const LOG = require('./helpers/logger');
+LOG.info("Log configured sucessfully");
 
 const express = require('express');
 const app = express();
 
-app.use(require("morgan")(( (app.get('env') === 'development') ? 'dev' : 'combined'), {"stream": logger.stream}));
+app.use(require("morgan")(( (app.get('env') === 'development') ? 'dev' : 'combined'), {"stream": LOG.stream}));
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
@@ -34,9 +15,9 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-const routes = require('./routes');
-
 /* ***************************** ROUTE CONFIGURATION */
+const routes = require('./routes');
+app.use('/v1/schedule', routes.schedule);
 
 
 /* HEALTH CHECK */
@@ -57,9 +38,9 @@ app.use((err, req, res, next) => {  // eslint-disable-line
   let st = (err.status || 500);
 
   if( st >= 500 ){
-    logger.error(err);
+    LOG.error(err);
   } else {
-    logger.warn(err);
+    LOG.warn(err);
   }
 
   res.status(st);
@@ -76,5 +57,5 @@ app.use((err, req, res, next) => {  // eslint-disable-line
 
 module.exports = {
   app: app,
-  logger: logger
+  logger: LOG
 };
