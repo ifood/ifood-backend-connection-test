3a75cb031630c627006069dc021a8dddcccc6df6
Rafael <trestini@gmail.com>
2017-12-11 09:31:11 -0200

    Esqueleto do frontend do comercial

---
+++ comercial-frontend/app/routes/login.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  model() {
+    return {
+      username: 'fakeauth',
+      password: 'anypass'
+    }
+  },
+
+  actions: {
+    auth(model) {
+      let session = JSON.stringify({
+        username: model.username,
+        timestamp: new Date()
+      });
+      sessionStorage.ifood_test = session;
+      this.transitionTo('home.realtime');
+    }
+  }
+
+});
---
+++ comercial-frontend/tests/unit/routes/home/restaurante-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:home/restaurante', 'Unit | Route | home/restaurante', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
