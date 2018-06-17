a798a047269ba9973dd595434348c7e25135cbf5
Walanem Figueiredo <walanem.figueiredo@gmail.com>
2018-06-17 20:08:54 +0000

    - fixing general cloud configuration (mongo, AWS credentials)

--- ifood-management/src/main/java/com/ifood/ifoodmanagement/IfoodManagementApplication.java
+++ ifood-management/src/main/java/com/ifood/ifoodmanagement/IfoodManagementApplication.java
@@ -2,12 +2,16 @@ package com.ifood.ifoodmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
 @ComponentScan(basePackages = "com.ifood")
+@EnableContextCredentials(
+        accessKey = "${ifoodproperties.aws.accessKey}",
+        secretKey = "${ifoodproperties.aws.secretKey}")
 public class IfoodManagementApplication {
 
     public static void main(String[] args) {
